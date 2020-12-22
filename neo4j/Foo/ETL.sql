736c88d07dbcafdedaaf3e6a5069d3414f7733a4
ANtonio García <jose.ramirez@cimat.mx>
2020-12-21 23:00:20 -0600

    se pode grafo prod-prod y agrega  archivo con 'variables' de productos

--- neo4j/Foo/ETL.sql
+++ neo4j/Foo/ETL.sql
@@ -5,9 +5,11 @@ create or replace table  `rmf2gcp.Desarrollo.productos_descripcion`
 as (
   SELECT  distinct *  from 
   (SELECT  1000000*(CASE des_Area WHEN 'Muebles' THEN 2  WHEN 'Ropa' THEN 1 ELSE 3 END) + 100000 * idu_DepartamentoCodigo+1000 *idu_ClaseCodigo + idu_FamiliaCodigo as id_Familia, 
-          des_Departamento as Departamento , des_Clase as Clase, des_Familia as Familia, des_Categoria as Categoria, des_Subcategoria as Subcategoria, 
-          idu_ArticuloCodigo, des_Articulo
-        FROM `rmf2gcp.Desarrollo.productos_descripcion_cruda`) as a
+          des_Departamento as Departamento , des_Clase as Clase, des_Familia as Familia, des_Categoria as Categoria, des_Subcategoria as Subcategoria--, 
+          --idu_ArticuloCodigo--, des_Articulo
+        FROM `rmf2gcp.Desarrollo.productos_descripcion_cruda`
+        WHERE des_Area in ('Muebles' , 'Ropa')
+        ) as a
         order by id_Familia ); 
 
 
